3bc56cdd6993bff62602710bc85638484110ac04
MohamedSaidMuse <51102939+MohamedSaidMuse@users.noreply.github.com>
2024-06-12 16:13:36 -0700

    Describe your changes here

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hi I am here to present"/>
     <WebhookHandler />
   </div>
 </template>
