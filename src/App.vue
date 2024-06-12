301037e2c0be94145faac4e9c92805b63c0b13dc
MohamedSaidMuse <51102939+MohamedSaidMuse@users.noreply.github.com>
2024-06-12 16:47:43 -0700

    new

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/1.png">
+    <img alt="Vue logo" src="./assets/logi.png">
     <HelloWorld msg="Hi I am here to present"/>
     <WebhookHandler />
   </div>
